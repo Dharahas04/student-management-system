ed837ebdd2bcd007102f05e70f332bd6ba74934f
Sai Dharahas Rao <dharahas04@gmail.com>
2026-02-16 11:49:03 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/student/student_management/entity/Student.java
@@ -0,0 +1,20 @@
+package com.student.student_management.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String course;
+}
