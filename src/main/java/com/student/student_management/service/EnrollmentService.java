8c51b5d5e0813ebe7da9ea3dab465e4c3a4421cb
Sai Dharahas Rao <dharahas04@gmail.com>
2026-02-17 09:53:22 +0530

    feat: implemented Enrollment service logic

---
+++ src/main/java/com/student/student_management/service/EnrollmentService.java
@@ -0,0 +1,37 @@
+package com.student.student_management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.student.student_management.entity.*;
+import com.student.student_management.repository.*;
+import com.student.student_management.dto.EnrollmentRequest;
+
+@Service
+public class EnrollmentService {
+
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public Enrollment enrollStudent(EnrollmentRequest request) {
+
+        Student student = studentRepository.findById(request.getStudentId())
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+
+        Course course = courseRepository.findById(request.getCourseId())
+                .orElseThrow(() -> new RuntimeException("Course not found"));
+
+        Enrollment enrollment = new Enrollment();
+        enrollment.setStudent(student);
+        enrollment.setCourse(course);
+        enrollment.setSemester(request.getSemester());
+
+        return enrollmentRepository.save(enrollment);
+    }
+}
