711f956653ea6e17df0cb0d0ce8652325b62e5e6
Sai Dharahas Rao <dharahas04@gmail.com>
2026-02-17 09:30:53 +0530

    feat: added Course entity with getters and setters

---
+++ src/main/java/com/student/student_management/entity/Course.java
@@ -0,0 +1,51 @@
+package com.student.student_management.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String courseName;
+    private int credits;
+
+    @OneToMany(mappedBy = "course")
+    @JsonIgnore
+    private List<Enrollment> enrollments;
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public List<Enrollment> getEnrollments() {
+        return enrollments;
+    }
+
+    public void setEnrollments(List<Enrollment> enrollments) {
+        this.enrollments = enrollments;
+    }
+}
